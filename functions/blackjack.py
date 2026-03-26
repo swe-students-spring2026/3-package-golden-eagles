799f905edaeab7d5a92a5319af9cb08188aea0b3
OverYander <gavinchen8706@gmail.com>
2026-03-25 21:03:43 -0400

    initial class and test

---
+++ functions/blackjack.py
@@ -0,0 +1,8 @@
+class Card():
+    def __init__(self, suit, num):
+        self.suit = suit
+        self.num = num
+
+# def blackjack():
+#     # waste time with a game of blackjack
+    
---
+++ tests/test_blackjack.py
@@ -0,0 +1,22 @@
+import pytest
+from functions.blackjack import Card
+
+# pytest tests/test_blackjack.py
+
+def test_card_creation():
+    # test card creation 
+    heart = Card("Hearts", 5)
+    assert heart.suit == "Hearts"
+    assert heart.num == 5
+
+    diamond = Card("Diamonds", 7)
+    assert diamond.suit == "Diamonds"
+    assert diamond.num == 7
+
+    club = Card("Clubs", 1)
+    assert club.suit == "Clubs"
+    assert club.num == 1
+
+    spade = Card("Spades", 9)
+    assert spade.suit == "Spades"
+    assert spade.num == 9
