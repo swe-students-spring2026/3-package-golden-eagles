a4789ffbd9fa9ececd93f6b08f975b43135c9a76
KyleC55 <kc5371@nyu.edu>
2026-03-26 20:36:02 -0400

    Core Function Setups

---
+++ Minefield-Game/core.py
@@ -0,0 +1,31 @@
+import random 
+
+def create_board(rows, cols, num_mines):
+    """
+    
+    """
+    return 
+
+def reveal_board(rows, cols):
+    """
+    
+    """
+    return
+
+def count_adjacent_mines(board, row, col):
+    """
+    
+    """
+    return
+
+def reveal_cell(board, row, col):
+    """
+    
+    """
+    return
+
+def check_win(board):
+    """
+    
+    """
+    return
