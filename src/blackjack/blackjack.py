876499df55bfd6f20138b8b6b0c1e467f1aafa92
OverYander <gavinchen8706@gmail.com>
2026-03-28 19:52:35 -0400

    print card helper

--- src/blackjack/blackjack.py
+++ src/blackjack/blackjack.py
@@ -10,14 +10,40 @@ def start_blackjack(deck):
     player_card2 = Card.pick_card(deck)
     dealer_card_hidden = Card.pick_card(deck)
 
-    print("Dealers:")
-    print(dealer_card_shown.print_card() + Card.print_blank())
+    player_cards = [player_card1, player_card2]
+    dealer_cards = [dealer_card_shown]
+    print_table(player_cards, dealer_cards, True)
+
+def print_table(player_cards, dealer_cards, players_turn):
+    if(players_turn):
+        print("Dealers:")
+        for card in dealer_cards:
+            print(card.print_card())
+        print(Card.print_blank())
+    else:
+        print("Dealers:")
+        for card in dealer_cards:
+            print(card.print_card())
 
     print("Your cards:")
-    print(player_card1.print_card() + player_card2.print_card())
+    for card in player_cards:
+        print(card.print_card())
 
+def stand_hit(player_total, deck):
+    while True:
+        print("Hit or Stand? (H-hit/X-stand)")
+        x = input()
 
+        if x.upper() == "H":
+            new_card = Card.pick_card(deck)
+            player_total += new_card.num
+            print("You drew:")
+            print(new_card.print_card())
 
+        elif x.upper() == "X":
+            return "stand"
+        else:
+            print("Invalid input: Enter 'H' for hit or 'X' for stand")
 
 start_blackjack(Card.generate_deck())
 # python src/blackjack/blackjack.py
