d98fc698064c4fd1f00ef526d7f212ea962176bb
KyleC55 <kc5371@nyu.edu>
2026-03-29 14:26:13 -0400

    Fixed pylint errors for minefield/sweeper

--- src/minefield/__init__.py
+++ src/minefield/__init__.py
@@ -1,3 +1,5 @@
+"""This package contains the core logic and game loop for the Minefield game."""
+
 from .core import (
     create_board,
     reveal_board,
--- src/minefield/__main__.py
+++ src/minefield/__main__.py
@@ -1,3 +1,7 @@
+"""This is the entry point for the Minefield game. 
+It imports the play_game function from the game module and starts the game loop when executed.
+"""
+
 from .game import play_game
 
 play_game()
